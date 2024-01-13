78f168e2c8e53a2087d0088c3ba3ca6b73123909
yarinmatmoni <yarinmatmoni@gmail.com>
2024-01-13 03:06:57 +0200

    option to cobain reducers

---
+++ src/store/reducers/user.reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+	user: {},
+};
+
+export const userReducer = (state = initialState, action = {}) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,13 @@
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, combineReducers } from 'redux';
+import { userReducer } from './reducers/user.reducer';
 import { storyReducer } from './reducers/story.reducer';
 
+const rootReducer = combineReducers({
+	userReducer: userReducer,
+	storyReducer: storyReducer,
+});
+
 export const store = createStore(
-	storyReducer,
+	rootReducer,
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
