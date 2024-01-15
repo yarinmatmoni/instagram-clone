6289e0a6aaadd5d64fb30023959c3d8554bb192a
yarinmatmoni <yarinmatmoni@gmail.com>
2024-01-16 00:19:02 +0200

    add basic style - header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
+import Logo from '../assets/svgs/logo-instagram.svg';
+import Create from '../assets/svgs/btns/create.svg';
+
 const header = () => {
-	return <div className='header'>header</div>;
+	return (
+		<div className='header'>
+			<img src={Logo} alt='logo' className='logo' />
+			<img src={Create} alt='crate' />
+		</div>
+	);
 };
 
 export default header;
