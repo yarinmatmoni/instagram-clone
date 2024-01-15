b4e80f71127cd6c929e626629c5978be29d1a6fe
yarinmatmoni <yarinmatmoni@gmail.com>
2024-01-15 12:33:48 +0200

    wide-layout navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import Search from '../assets/svgs/btns/search.svg';
 import Message from '../assets/svgs/btns/messenger.svg';
 import Heart from '../assets/svgs/btns/heart.svg';
 import Avatar from '../assets/images/avatar.png';
-import Logo from '../assets/svgs/logo-camera.svg';
 import Explore from '../assets/svgs/btns/explore.svg';
 import Create from '../assets/svgs/btns/create.svg';
 import Menu from '../assets/svgs/btns/menu2.svg';
@@ -13,7 +12,7 @@ const Navbar = () => {
 	return (
 		<nav className='navbar'>
 			<NavLink to='/' className='logo'>
-				<img src={Logo} alt='logo' />
+				<div className='logo-image'></div>
 			</NavLink>
 			<div className='navbar-links'>
 				<NavLink to='/' className='mobile-display'>
@@ -47,6 +46,7 @@ const Navbar = () => {
 			</div>
 			<div className='menu'>
 				<img src={Menu} alt='menu' />
+				<div className='name'>More</div>
 			</div>
 		</nav>
 	);
