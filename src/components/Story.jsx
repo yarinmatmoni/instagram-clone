2b2b27f7e9dff34664d575d858b3ece6c7d4e159
yarinmatmoni <yarinmatmoni@gmail.com>
2024-01-16 17:48:43 +0200

    story component basic style

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,6 +1,11 @@
+import { Link } from 'react-router-dom';
 import Avatar from '../assets/images/avatar.png';
 import Image from '../assets/images/image-placeholder.png';
 import Menu from '../assets/svgs/btns/menu3.svg';
+import Heart from '../assets/svgs/btns/heart.svg';
+import Comment from '../assets/svgs/btns/comment.svg';
+import Share from '../assets/svgs/btns/share.svg';
+import Save from '../assets/svgs/btns/saved.svg';
 
 const Story = ({ story }) => {
 	return (
@@ -14,8 +19,30 @@ const Story = ({ story }) => {
 				<img src={Image} alt='story image' />
 			</div>
 			<div className='story-details'>
-				<div className='story-likes'>likes</div>
-				<div className='story-comments'>comments</div>
+				<div className='story-options'>
+					<div className='story-options-left'>
+						<img src={Heart} alt='like' />
+						<img src={Comment} alt='comment' />
+						<img src={Share} alt='share' />
+					</div>
+					<img src={Save} alt='save' />
+				</div>
+				{story.likedBy.length > 0 && (
+					<Link to='/' className='story-likes'>
+						{story.likedBy.length} Likes
+					</Link>
+				)}
+
+				<div className='story-description'>
+					<Link to='/'>{story.by.fullName}</Link>
+					<div className='txt'>{story.txt}</div>
+				</div>
+				{story.comments.length > 0 && (
+					<Link to='/' className='story-comments'>
+						View all {story.comments.length} comments
+					</Link>
+				)}
+				<div className='story-time-details'>5 hours ago</div>
 			</div>
 		</div>
 	);
